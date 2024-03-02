05969c4b82822539db6483de3e01042e56a375e9
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-03-02 13:06:07 -0600

    Update IDEConfig.sh

--- IDEConfig.sh
+++ IDEConfig.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+#################################################################################################
+# Author: Nicholas Fisher
+# Date: March 3rd 2024
+# Description of Script
+# This script is used to to install a Visual studio code IDE for Kali Linux. In order for the script
+# to work please go to https://code.visualstudio.com/download to download the appropriate files.
+# And place this script in the same directory as the downloaded file.
+#################################################################################################
 apt-get install code
 apt-get install -f ./code*

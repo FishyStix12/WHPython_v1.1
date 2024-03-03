3b6e9262aa496d142aaf020ce14954b39dc28dc5
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-03-03 17:15:08 -0600

    Update tport_sniffer.py

--- tport_sniffer.py
+++ tport_sniffer.py
@@ -1,3 +1,26 @@
+#! /usr/bin/python
+#################################################################################################
+# Author: Nicholas Fisher
+# Date: March 4th 2024
+# Description of Script
+# This Python script utilizes the Scapy library to sniff network packets and detect potential 
+# email credentials being transmitted in plaintext. It allows the user to specify TCP port 
+# filters to focus on specific network traffic. When a packet containing 'user' or 'pass' 
+#in its payload is detected, the script prints the destination IP address and the payload, 
+# which may include email credentials. This tool can be used for network security auditing or 
+# monitoring purposes to identify and mitigate potential credential leaks.
+# Example Usuage:
+# Do you want to add more filters? (yes/no): yes
+# Enter the port number: 25
+# Do you want to add more filters? (yes/no): yes
+# Enter the port number: 110
+# Do you want to add more filters? (yes/no): no
+# Applying filter: tcp port 25 or tcp port 110
+# Example output:
+# [*] Destination: 192.168.1.1
+# [*] USER myemail@example.com
+#################################################################################################
+#imports the socket and threading modules for python 3.
 from scapy.all import sniff, TCP, IP
 
 def packet_callback(packet):

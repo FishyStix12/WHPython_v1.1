1cb77e220703108209e275f3b0db9f155cf63f6e
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-03-05 00:10:11 -0600

    Update push_trojan_modules.sh

--- push_trojan_modules.sh
+++ push_trojan_modules.sh
@@ -3,12 +3,14 @@
 # Author: Nicholas Fisher
 # Date: March 4th 2024
 # Description of Script
-# This script automates the process to push new features into the active Trojan on Github.
+# This script automates the process to push new updates into the active Trojan on Github.
 # To ensure this script works please place it in the <trojan_name> directory. You will need your
 # Github username and password to push the Trojan update.
 #################################################################################################
 git add .
-git commit -m "Adds New modules"
+echo -n "What is this update for: " # Ex: Adds simple configuration, Adds new modules
+read user_input
+git commit -m f"{user_input}"
 git push origin master
 # Enter username:
 # Enter password:

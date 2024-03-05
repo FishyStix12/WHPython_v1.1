848f6beaf5cd31f250f823c7edc253eb73e9501b
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-03-05 00:32:07 -0600

    Create data_pull.sh

---
+++ data_pull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+#!/bin/bash
+#################################################################################################
+# Author: Nicholas Fisher
+# Date: March 5th 2024
+# Description of Script
+# This script pulls the results of the running Trojan Modules.
+#################################################################################################
+git pull origin master

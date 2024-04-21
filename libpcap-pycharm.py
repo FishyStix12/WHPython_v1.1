0447c7fec7e692196e0a6695b855eceae7dc3c2f
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-04-21 14:04:43 -0500

    Create libpcap-pycharm.py

---
+++ libpcap-pycharm.py
@@ -0,0 +1,6 @@
+# This script is to set up the libpcap for pycharm, and to upgrade the pycharm scapy package
+# By: Nicholas Fisher
+# Date: 4/21/2024
+from scapy.packet import Packet
+from scapy.fields import IPField, XShortField, XByteField
+from scapy.layers.inet import TCP

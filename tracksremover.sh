a21455a58f8419a5871850967ec2cf9b395453a8
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-07-19 23:52:14 -0400

    Update tracksremover.sh

--- tracksremover.sh
+++ tracksremover.sh
@@ -1,3 +1,16 @@
-
+#!/bin/bash
+#################################################################################################
+# Author: Nicholas Fisher
+# Date: March 2nd 2024
+# Description of Script
+#  This Bash script is designed to securely delete the command history from the current user's 
+# shell session. It begins by using the `shred` command to overwrite the `.bash_history` 
+# file multiple times and then remove it, ensuring that the deleted data cannot be easily
+# recovered. Following this, the script creates a new empty `.bash_history` file and clears
+# the current session's history using the `history -c` command. Finally, it exits the shell.
+# This sequence of commands ensures both the secure deletion of past command history and the
+# prevention of any residual data from the current session, enhancing overall privacy and
+# security.
+#################################################################################################
 
 shred -/.bash_history&& cat /dev/null > .bash_history && history -c && exit
